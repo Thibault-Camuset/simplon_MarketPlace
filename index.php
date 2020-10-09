9205d201fa2e9876f56321f536a258d064d3bef5
Thibault-Camuset <thibault.camuset@gmail.com>
2020-10-09 15:14:27 +0200

    structure de base du site

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    
+    <script type="Text/Javascript" src="JS/script.js"></script>
+</body>
+</html>
