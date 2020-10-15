5d84b9ac8483202321d05604b2234d1534009366
Thibault-Camuset <thibault.camuset@gmail.com>
2020-10-15 15:29:55 +0200

    modif JQuery

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+$backofficediv = $('#back-office-container');
+$backofficebutton = $('#switch-back-office');
+$frontbutton = $('#switch-front');
+$pagecontainer = $('#page-container');
+
+$backproducts = $('#back-office-products');
+$backvendors = $('#back-office-vendors');
+$backcategories = $('#back-office-categories');
+$backbrands = $('#back-office-brands');
+
+$backproductsdisplay = $('#back-office-products-display');
+$backvendorsdisplay = $('#back-office-vendors-display');
+$backcatagoriesdisplay = $('#back-office-categories-display');
+$backbrandsdisplay = $('#back-office-brands-display');
+
+
+
+$backofficebutton.click(function() {
+    $pagecontainer.addClass("hidden");
+    $backofficediv.removeClass("hidden");
+});
+
+$frontbutton.click(function() {
+    $pagecontainer.removeClass("hidden");
+    $backofficediv.addClass("hidden");
+});
+
+$backproducts.click(function() {
+    $backofficediv.removeClass("hidden");
+    $backproductsdisplay.addClass("hidden");
+});
--- index.php
+++ index.php
@@ -8,18 +8,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
 
     <div id="page-container">
-        <input type="button" id="switch-back-office"/>
-        <div id="products-page"></div>
+
+        <div id="formulaire">
+            <input type="text" id="search-input" placeholder="Rechercher un produit"/>
+            <input type="button" id="search-button" value="Envoyer"/>
+            <select id="select-categories">
+                <option>Blabla</option>
+                <option>Blabla</option>
+                <option>Blabla</option>
+                <option>Blabla</option>
+            </select>
+            <input type="button" id="switch-back-office" value="B"/>
+        </div>
+
+        <div id="products-page">
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+            <div class="popo"></div>
+        </div>
         <div id="product-details-page" class="hidden"></div>
     </div>
 
     <div id="back-office-container" class="hidden">
+    <input type="button" id="switch-front" value="F"/>
+    <div id="back-office-products">Products</div>
+    <div id="back-office-vendors">Vendors</div>
+    <div id="back-office-categories">Categories</div>
+    <div id="back-office-brands">Brands</div>
     </div>
+
+    <div id="back-office-products-display" class="hidden"></div>
+    <div id="back-office-vendors-display" class="hidden"></div>
+    <div id="back-office-categories-display" class="hidden"></div>
+    <div id="back-office-brands-display" class="hidden"></div>
 
 
     <script type="Text/Javascript" src="JS/script.js"></script>
